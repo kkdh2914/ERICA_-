0e3179d7286be9e7cfc3877b35c3d3f6e3f02aaa
JeAxyz <jejiho6302@gmail.com>
2019-12-05 14:26:19 +0900

    start

---
+++ updown.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define randomize() srand((unsigned)time(NULL))
+#define random(n) (rand() % (n))
+
+int run_game() {
+	int nRandom, count, nlnput;
+	for (nRandom = random(100), count = 1;; ++count)
+	{
+		printf("Please enter a number\n");
+
+		scanf_s("%d",&nlnput);
+
+		if (nRandom == nlnput)
+			break;
